9c7055b9471a9b63b5d1b6be1d50f7f31a8d9ce1
dreamhead <dreamhead.cn@gmail.com>
2012-09-25 16:19:30 +0800

    added antlr support

--- lib/1step/command.rb
+++ lib/1step/command.rb
@@ -4,6 +4,7 @@ module Firstep
   class Command < Thor
   	desc 'new project_name', 'create a new project'
   	method_option :war, :default => false, :aliases => "-w", :desc => "create Java web project"
+    method_option :antlr, :default => false, :aliases => "--antlr", :desc => "create Java antlr project" 
   	def new(project_name)
       project_options = {
         :language => :java, 
@@ -12,6 +13,7 @@ module Firstep
       }
 
       project_options[:type] = :acceptance if options[:war]
+      project_options[:type] = :antlr if options[:antlr]
       target_directory = '.'
       ProjectGenerator.new(project_options).create(project_name, target_directory)
   	end
